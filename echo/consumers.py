412a6d52ea09d7792d200e15f8a5049b897f87ee
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-05-25 21:43:44 +0330

    add echo image work with vyte data

--- echo/consumers.py
+++ echo/consumers.py
@@ -8,5 +8,8 @@ class EnchoConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         pass
 
-    async def receive(self, text_data):
-        await self.send(text_data=text_data)
+    async def receive(self, text_data=None ,bytes_data=None):
+        if text_data:
+            await self.send(text_data=text_data)
+        elif bytes_data:
+            await self.send(bytes_data=bytes_data)    
--- echo/urls.py
+++ echo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, echo_image
 
 
 
 urlpatterns = [
-    path("",  index, name="index")
+    path("",  index, name="index"),
+    path("image/", echo_image, name="echo_image")
+
 ]
